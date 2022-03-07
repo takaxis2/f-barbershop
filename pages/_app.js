9dd664da7140a9626e42e9b54230882df8b5907d
takaxis2 <axis1130@gmail.com>
2022-03-08 00:58:37 +0900

    set up

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import CssBaseline  from "@mui/material/CssBaseline";
+
+function MyApp({ Component, pageProps }) {
+
+  return( 
+    <>
+    <CssBaseline />
+    <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
